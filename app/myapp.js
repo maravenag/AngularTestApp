96af1215e85d6d47faeda4269827ddc359493b46
maravenag <aravenagamboa@gmail.com>
2017-08-01 15:53:11 -0400

    add components

--- app/myapp.js
+++ app/myapp.js
@@ -5,16 +5,39 @@ app.config(function ($interpolateProvider) {
     $interpolateProvider.endSymbol(']]');
 });
 
+app.controller('nameController', function () {
+    ctr = this;
+    ctr.name = "Matias";
+
+    this.changeName = function (ctr) {
+        ctr.name = "Felipe";
+    }
+});
+
+
 app.component("hola", {
     template: ' <h3> Hola [[ $ctrl.myName ]]</h3>' +
-              '<button type="button" ng-click=$ctrl.buttonClicked()> Button</button>',
+    '<button type="button" ng-click=$ctrl.buttonClicked()> Button</button>',
     bindings: '=',
     controller: function () {
-        this.myName = "Matias"
-
+        this.myName = "Matias2";
         this.buttonClicked = function () {
-            console.log("clicked!!");
+            this.myName = this.myName + "hola";
         }
     }
 });
 
+app.component("hola2", {
+    template: `
+            <h2> Este es mi otro [[ $ctrl.name ]] </h2>
+            <h5 ng-click=$ctrl.printName() > Click aquí :D </h5>
+            `,
+    bindings: {
+        name: '<'
+    },
+    controller: function () {
+        this.printName = function () {
+            console.log(this.name);
+        }
+    }
+});
